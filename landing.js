b25cebf8a60e63ebbb7732861f39ef4e0f5d5ab5
JP Bulman <jpbulman@wpi.edu>
2018-11-26 19:08:26 -0500

    Fixed the Go button so it requires both fields

--- landing.js
+++ landing.js
@@ -2,12 +2,8 @@ function goAction(){
     var scheduleVal = document.getElementById("scheduleID").value;
     var organizerVal = document.getElementById("organizerID").value;
 
-    if((scheduleVal==""&&organizerVal=="")){
+    if(scheduleVal==""||organizerVal==""){
         alert("Please fill in the required fields");
-    }
-
-    if(scheduleVal!=""&&organizerVal!=""){
-        alert("You can only enter in one field");
     }
 
 }
