7c8cdaeb056b1542f11e62b83e5c8b23811120b4
Brittany Goldstein <britg321@gmail.com>
2018-12-04 18:48:48 -0500

    Lambda function for participant's viewing schedule

--- cs3733kajam/src/main/java/com/amazonaws/lambda/demo/GetTimeSlotParticipant.java
+++ cs3733kajam/src/main/java/com/amazonaws/lambda/demo/GetTimeSlotParticipant.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.UUID;
@@ -22,10 +21,14 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.google.gson.Gson;
 
+import db.MeetingDAO;
 import db.ScheduleDAO;
+import db.TimeSlotDAO;
+import model.Meeting;
 import model.Schedule;
+import model.TimeSlot;
 
-public class GetScheduleHandler implements RequestStreamHandler {
+public class GetTimeSlotParticipant implements RequestStreamHandler{
 	public LambdaLogger logger = null;
 
 	// handle to our s3 storage
@@ -34,13 +37,12 @@ public class GetScheduleHandler implements RequestStreamHandler {
 
 	boolean useRDS = true;
 	
-	
-	Schedule getSchedule(UUID id) throws Exception{
+	TimeSlot getTimeSlot(UUID id, LocalDate date, LocalTime startTime) throws Exception{
 		if (logger != null) { logger.log("in getSchedule"); }
-		ScheduleDAO dao = new ScheduleDAO();
+		TimeSlotDAO dao = new TimeSlotDAO();
 		
-		// check if present
-		Schedule exist = dao.getSchedule(id);
+		// check if present 
+		TimeSlot exist = dao.getTimeSlotByDateTime(id, date, startTime);
 		if (exist == null) {
 			throw new NullPointerException();
 		} else {
@@ -48,19 +50,21 @@ public class GetScheduleHandler implements RequestStreamHandler {
 		}
 	}
 	
+	Meeting getMeeting(UUID id) throws Exception{
+		if (logger != null) { logger.log("in getSchedule"); }
+		MeetingDAO dao = new MeetingDAO();
+		
+		// check if present 
+		Meeting exist = dao.getMeetingByTimeSlotID(id);
+		if (exist == null) {
+			throw new NullPointerException();
+		} else {
+			return exist;
+		}
+	}
 	
-
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-    	
-    	// FAKE fix this
-//    	try {
-//			DatabaseUtil.connect();
-//			System.out.println("SUCCESS!");
-//		} catch (Exception e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
     	
     	LambdaLogger logger = context.getLogger();
 		logger.log("Loading Java Lambda handler of RequestStreamHandler");
@@ -73,7 +77,7 @@ public class GetScheduleHandler implements RequestStreamHandler {
 		JSONObject responseJson = new JSONObject();
 		responseJson.put("headers", headerJson);
 		
-		GetScheduleResponse response;
+		GetTimeslotParticipantResponse response;
 		
 		String body;
 		boolean processed = false;
@@ -99,31 +103,61 @@ public class GetScheduleHandler implements RequestStreamHandler {
 		} catch (ParseException pe) {
 			logger.log(pe.toString());
 			//TODO: add more parameters
-			response = new GetScheduleResponse("", LocalTime.now(), LocalTime.now(), LocalDate.now(), LocalDate.now(), 0, UUID.randomUUID(), 422);  // unable to process input
+			response = new GetTimeslotParticipantResponse(UUID.randomUUID(), LocalDate.now(), LocalTime.now(), false, false, "", 422);  // unable to process input
 	        responseJson.put("body", new Gson().toJson(response));
 	        processed = true;
 	        body = null;
 		}
 		
 		if (!processed) {
-			GetScheduleRequest req = new Gson().fromJson(body, GetScheduleRequest.class);
+			GetTimeslotRequest req = new Gson().fromJson(body, GetTimeslotRequest.class);
 			logger.log(req.toString());
 			
 			String respError = "";
-
-			Schedule s;
+			
+			UUID id;
 			try {
-				s = getSchedule(UUID.fromString(req.arg1));
-			} catch (Exception e) {
-				s = null;
+				id = UUID.fromString(req.arg1);
+			} catch(Exception e) {
+				id = null;
 				respError += "Invalid ID ";
 			}
 			
-			int secretCode = 0;
+			int year = 0;
+			int month = 0;
+			int day = 0;
+			int hour = 0;
+			int min = 0;
 			try {
-				secretCode = Integer.parseInt(req.arg2);
-			}catch(NumberFormatException e) {
-				respError += "Invalid format for secretCode: " + secretCode + "  req.arg1 " + req.arg1 + " req.arg2 " + req.arg2 + " body: " + body + " ";
+				year = Integer.parseInt(req.arg2);
+				month = Integer.parseInt(req.arg3);
+				day = Integer.parseInt(req.arg4);
+				hour = Integer.parseInt(req.arg5);
+				min = Integer.parseInt(req.arg6);
+			}catch(Exception e) {
+				respError += "Invalid number format ";
+			}
+
+			LocalDate date = null;
+			try {
+				date = LocalDate.of(year, month, day);
+			}catch (Exception e) {
+				respError += "Invalid date ";
+			}
+			
+			LocalTime startTime = null;
+			try {
+				startTime = LocalTime.of(hour, min);
+			}catch(Exception e) {
+				respError += "Invalid startTime ";
+			}
+			
+			TimeSlot s;
+			try {
+				s = getTimeSlot(id, date, startTime);
+			} catch (Exception e) {
+				s = null;
+				respError += "No timeslot matches given parameters ";
 			}
 			
 			// compute proper response
@@ -131,12 +165,23 @@ public class GetScheduleHandler implements RequestStreamHandler {
 				ErrorResponse resp = new ErrorResponse(respError, 400);
 				responseJson.put("body", new Gson().toJson(resp));
 			}else {
-				if(s.secretCode == secretCode) {
-					GetScheduleResponse resp = new GetScheduleResponse(s.name, s.startTime, s.endTime, s.startDate, s.endDate, s.duration, s.id, 200);
+				if(s.isFree == true) {
+					GetTimeslotParticipantResponse resp = new GetTimeslotParticipantResponse(id, date, startTime, false, true, "NONE", 200);
 					responseJson.put("body", new Gson().toJson(resp));
 				}else {
-					ErrorResponse resp = new ErrorResponse("Invalid secretCode", 400);
-					responseJson.put("body", new Gson().toJson(resp));
+					Meeting m;
+					try {
+						m = getMeeting(s.id);
+					}catch(Exception e) {
+						m = null;
+					}
+					if(m != null) {
+						GetTimeslotParticipantResponse resp = new GetTimeslotParticipantResponse(id, date, startTime, false, false, m.name, 200);
+						responseJson.put("body", new Gson().toJson(resp));
+					}else {
+						GetTimeslotParticipantResponse resp = new GetTimeslotParticipantResponse(id, date, startTime, true, false, "NONE", 200);
+						responseJson.put("body", new Gson().toJson(resp));
+					}
 				}
 			}
 		}
