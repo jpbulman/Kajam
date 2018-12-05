cce018a90ed079d76369372da2859f5859a45ebe
Andrew <andrewmarkoski@gmail.com>
2018-12-05 13:47:15 -0500

    Fixed ScheduleTest

--- cs3733kajam/src/test/java/model/ScheduleTest.java
+++ cs3733kajam/src/test/java/model/ScheduleTest.java
@@ -17,6 +17,7 @@ public class ScheduleTest {
 	    Schedule s = new Schedule(UUID.randomUUID(), "test", 123, 30, 
 	    		LocalTime.of(12, 0), LocalTime.of(16, 0), LocalDate.of(2018, 12, 03),
 	    		LocalDate.of(2018, 12, 10),new Timestamp(System.currentTimeMillis()));
+	    s.generateTimeSlots();
 	    assertEquals(s.timeSlots.size(),48);
 	}
 	
