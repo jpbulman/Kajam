6123e8eda7c1a75ccab6ad46b172b59c95f0b883
aspielman1 <aspielma@gmail.com>
2018-12-05 14:33:30 -0500

    created Organizer deleteMeetings and test

---
+++ cs3733kajam/src/main/java/com/amazonaws/lambda/demo/OrgDeleteMeetingHandler.java
@@ -0,0 +1,145 @@
+package com.amazonaws.lambda.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.UUID;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.google.gson.Gson;
+
+import db.MeetingDAO;
+import model.Meeting;
+
+public class OrgDeleteMeetingHandler implements RequestStreamHandler {
+
+
+	public LambdaLogger logger = null;
+
+	// handle to our s3 storage
+	private AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+			.withRegion("us-east-2").build();
+	
+	
+	boolean deleteMeeting(UUID id, UUID timeSlotid, String name, int secretCode) throws Exception {
+		if (logger != null) { logger.log("in createMeeting"); }
+		MeetingDAO dao = new MeetingDAO();
+		Meeting exist;
+		// check if present
+		exist = dao.getMeeting(id);
+		if (exist == null) {
+			return false;
+		} else {
+			dao.deleteMeeting(exist);
+			return true;
+		}
+	}
+	
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+    	
+    	LambdaLogger logger = context.getLogger();
+		logger.log("Loading Java Lambda handler of RequestStreamHandler");
+
+		JSONObject headerJson = new JSONObject();
+		headerJson.put("Content-Type",  "application/json");  // not sure if needed anymore?
+		headerJson.put("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+	    headerJson.put("Access-Control-Allow-Origin",  "*");
+	    
+		JSONObject responseJson = new JSONObject();
+		responseJson.put("headers", headerJson);
+		
+		DeleteMeetingResponse response;
+		
+		String body;
+		boolean processed = false;
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			JSONParser parser = new JSONParser();
+			JSONObject event = (JSONObject) parser.parse(reader);
+			logger.log("event:" + event.toJSONString());
+			
+			String method = (String) event.get("httpMethod");
+			if (method != null && method.equalsIgnoreCase("OPTIONS")) {
+				logger.log("Options request");
+				ErrorResponse r = new ErrorResponse("DONE (OPTIONS)", 200);
+				responseJson.put("body", new Gson().toJson(r));
+		        processed = true;
+		        body = null;
+			}else {
+				body = (String)event.get("body");
+				if (body == null) {
+					body = event.toJSONString();  // this is only here to make testing easier
+				}
+			}
+		} catch (ParseException pe) {
+			logger.log(pe.toString());
+			//TODO: add more parameters
+			response = new DeleteMeetingResponse(null, null, "", 0, 400);  // unable to process input
+	        responseJson.put("body", new Gson().toJson(response));
+	        processed = true;
+	        body = null;
+		}
+
+		if (!processed) {
+			DeleteMeetingRequest req = new Gson().fromJson(body, DeleteMeetingRequest.class);
+			logger.log(req.toString());
+			
+			String respError = "";
+			UUID val1 = null; // timeslot
+			int val2 = 0;     // secretcode
+			UUID val3 = null; // id
+			
+			Meeting m;
+			try {
+				val2 = Integer.parseInt(req.secretCode);
+			} catch (NumberFormatException e){
+				respError = "Invalid input format";
+			}
+			try {
+				val1 = UUID.fromString(req.timeSlotID);
+			} catch (Exception e) {
+				val1 = null;
+				respError = "Invalid input format";
+			}
+			try {
+				val3 = UUID.fromString(req.id);
+			} catch (Exception e) {
+				val3 = null;
+				respError = "Invalid input format";
+			}
+			
+			// compute proper response
+			if(respError.compareTo("") != 0) { // If there is an error in input
+				ErrorResponse resp = new ErrorResponse(respError, 400);
+				responseJson.put("body", new Gson().toJson(resp));
+			}else {
+				try {
+					if(deleteMeeting(val3, val1, req.name, val2)) {
+						DeleteMeetingResponse resp = new DeleteMeetingResponse(val3, val1, req.name, val2, 200);
+						responseJson.put("body", new Gson().toJson(resp));
+					}
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+				}
+			}
+		}
+        logger.log("end result:" + responseJson.toJSONString());
+        logger.log(responseJson.toJSONString());
+        OutputStreamWriter writer = new OutputStreamWriter(output, "UTF-8");
+        writer.write(responseJson.toJSONString());  
+        writer.close();
+    }
+}
---
+++ cs3733kajam/src/test/java/com/amazonaws/lambda/demo/OrgDeleteMeetingTest.java
@@ -0,0 +1,76 @@
+package com.amazonaws.lambda.demo;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import db.MeetingDAO;
+import model.Meeting;
+
+public class OrgDeleteMeetingTest {
+
+    private static final String SAMPLE_INPUT_STRING = "{\"foo\": \"bar\"}";
+    private static final String EXPECTED_OUTPUT_STRING = "{\"headers\":{\"Access-Control-Allow-Origin\":\"*\",\"Access-Control-Allow-Methods\":\"GET,POST,OPTIONS\",\"Content-Type\":\"application\\/json\"},\"body\":\"{\\\"error\\\":\\\"Invalid input format\\\",\\\"httpCode\\\":400}\"}";    
+    private static final String SAMPLE_INPUT_STRING3 = "{\n" + 
+    		"    \"id\": \"2c78a573-b5a7-46ad-814c-ff3d33808b9a\",\n" + 
+    		"    \"timeSlotID\": \"eea5770b-84ac-446f-bfdc-442f26fb2987\",\n" + 
+    		"    \"name\": \"name\",\n" + 
+    		"    \"secretCode\": \"88888\",\n" +
+    		"}";
+    Context createContext(String apiCall) {
+        TestContext ctx = new TestContext();
+        ctx.setFunctionName(apiCall);
+        return ctx;
+    }
+    
+    // Test create schedule with completely invalid inputs
+    @Test
+    public void testOrgDeleteMeetingHandler() throws IOException {
+    	OrgDeleteMeetingHandler handler = new OrgDeleteMeetingHandler();
+
+        InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING.getBytes());;
+        OutputStream output = new ByteArrayOutputStream();
+
+        handler.handleRequest(input, output, createContext("sample"));
+
+        // TODO: validate output here if needed.
+        String sampleOutputString = output.toString();
+        Assert.assertEquals(EXPECTED_OUTPUT_STRING, sampleOutputString);
+    }
+    
+    @Test
+    public void testOrgDeleteMeetingHandler2() throws IOException {
+    	MeetingDAO dao = new MeetingDAO();
+
+		UUID testID = UUID.fromString("2c78a573-b5a7-46ad-814c-ff3d33808b9a");
+		
+		UUID testSlot = UUID.fromString("eea5770b-84ac-446f-bfdc-442f26fb2987");
+		
+		Meeting meeting = new Meeting(testID, testSlot, "name", 88888);
+		try {
+			dao.addMeeting(meeting);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+		}
+		
+    	OrgDeleteMeetingHandler handler = new OrgDeleteMeetingHandler();
+        InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING3.getBytes());;
+        OutputStream output = new ByteArrayOutputStream();
+        
+        
+
+        handler.handleRequest(input, output, createContext("sample"));
+        
+        // TODO: validate output here if needed.
+        String sampleOutputString = output.toString();
+        Assert.assertTrue(sampleOutputString.contains("httpCode\\\":200"));
+    }
+}
