7e6d9701f09c6ba1d037c6a3035f31ef70e28593
Andrew <andrewmarkoski@gmail.com>
2018-12-12 12:07:49 -0500

    DeleteOldSchedule Request and response

--- cs3733kajam/src/main/java/com/amazonaws/lambda/demo/DeleteOldScheduleRequest.java
+++ cs3733kajam/src/main/java/com/amazonaws/lambda/demo/DeleteOldScheduleRequest.java
@@ -1,5 +1,15 @@
 package com.amazonaws.lambda.demo;
 
 public class DeleteOldScheduleRequest {
+	String arg1; // N days
 
+	public DeleteOldScheduleRequest(String arg1) {
+		super();
+		this.arg1 = arg1;
+	}
+
+	@Override
+	public String toString() {
+		return "DeleteOldScheduleRequest [arg1=" + arg1 + "]";
+	}
 }
--- cs3733kajam/src/main/java/com/amazonaws/lambda/demo/DeleteOldScheduleResponse.java
+++ cs3733kajam/src/main/java/com/amazonaws/lambda/demo/DeleteOldScheduleResponse.java
@@ -1,5 +1,23 @@
 package com.amazonaws.lambda.demo;
 
+import java.util.ArrayList;
+
+import model.Schedule;
+
 public class DeleteOldScheduleResponse {
 
+	ArrayList<Schedule> schedulesList;
+
+	public DeleteOldScheduleResponse(ArrayList<Schedule> schedulesList) {
+		super();
+		this.schedulesList = schedulesList;
+	}
+
+	@Override
+	public String toString() {
+		return "DeleteOldScheduleResponse [schedulesList=" + schedulesList + "]";
+	}
+	
+	
+	
 }
